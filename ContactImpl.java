3a9206605b4ad21ce9842b695ca94c6260e3336d
Hijiri Home <hijiribarrett@gmail.com>
2015-12-16 19:44:05 +0000

    changed constructor to initialise c_id private member, and edited getId() to return c_id

--- ContactImpl.java
+++ ContactImpl.java
@@ -1,20 +1,24 @@
 public class ContactImpl implements Contact {
-	
+	private int c_id;
 	
 	public ContactImpl(int id, String name, String notes) {
 		if(id<=0) {
 			throw new IllegalArgumentException();
-		}		
+		}
+		
+		this.c_id = id;
 	}
 	
 	public ContactImpl(int id, String name) {
 		if(id<=0) {
 			throw new IllegalArgumentException();
 		}
+		
+		this.c_id = id;
 	}
 	
 	public int getId() {
-		return 0;
+		return c_id;
 	}
 	
 	public String getName() {
