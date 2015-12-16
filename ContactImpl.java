ccbba8718746d18325386214f31084200ab7f3fb
Hijiri Home <hijiribarrett@gmail.com>
2015-12-16 19:30:05 +0000

    my commits aren't working

--- ContactImpl.java
+++ ContactImpl.java
@@ -2,10 +2,28 @@ public class ContactImpl implements Contact {
 	
 	
 	public ContactImpl(int id, String name, String notes) {
-		
+		if(id<=0) {
+			throw new IllegalArgumentException();
+		}
 	}
 	
 	public ContactImpl(int id, String name) {
 		
+	}
+	
+	public int getId() {
+		return 0;
+	}
+	
+	public String getName() {
+		return "";
+	}
+	
+	public String getNotes() {
+		return "";
+	}
+	
+	public void addNotes(String note) {
+		return;
 	}
 }
--- ContactShould.java
+++ ContactShould.java
@@ -10,7 +10,21 @@ public class ContactShould {
 	
 	@Test
 	public void createContact() {
-		contact = new ContactImpl();
+		contact = new ContactImpl(1, "Sam", "Not nice");
 		assertNotNull(contact);
+		
+		contact = new ContactImpl(2, "Jenna");
+		assertNotNull(contact);
+	}
+	
+	@Test
+	public void throwIllegalArgumentWhenIDLessThanOrEqualToZeroWithThreePerams() {
+		boolean isIllegal = false;		
+		try {
+			contact = new ContactImpl(0, "Sam", "Not nice");
+		} catch (IllegalArgumentException ex) {
+			isIllegal = true;
+		}
+		assertTrue(isIllegal);
 	}
 }
