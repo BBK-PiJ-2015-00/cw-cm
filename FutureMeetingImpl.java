6b828bf3242925038b3c54d9a35a61875efc15a0
Hijiri Home <hijiribarrett@gmail.com>
2015-12-17 16:14:53 +0000

    added fm_date private member, initialised it in constructor, and changed getDate() to return fm_date

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -3,9 +3,11 @@ import java.util.Set;
 
 public class FutureMeetingImpl implements FutureMeeting {
 	private int fm_id;
+	private Calendar fm_date;
 	
 	public FutureMeetingImpl(int id, Calendar date, Set<Contact> contacts) {
 		fm_id = id;
+		fm_date = date;
 	}
 	
 	
@@ -14,7 +16,7 @@ public class FutureMeetingImpl implements FutureMeeting {
 	}
 	
 	public Calendar getDate() {
-		return null;
+		return fm_date;
 	}
 	
 	public Set<Contact> getContacts() {
