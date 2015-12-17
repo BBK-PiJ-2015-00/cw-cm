6db41a5ef5c511f547db2e2a792b663a852bfac4
Hijiri Home <hijiribarrett@gmail.com>
2015-12-17 15:41:33 +0000

    implemented FutureMeeting and created constructor

---
+++ FutureMeetingImpl.java
@@ -0,0 +1,22 @@
+import java.util.Calendar;
+import java.util.Set;
+
+public class FutureMeetingImpl implements FutureMeeting {
+	
+	public FutureMeetingImpl(int id, Calendar date, Set<Contact> contacts) {
+		
+	}
+	
+	
+	public int getId() {
+		return -1;
+	}
+	
+	public Calendar getDate() {
+		return null;
+	}
+	
+	public Set<Contact> getContacts() {
+		return null;
+	}
+}
