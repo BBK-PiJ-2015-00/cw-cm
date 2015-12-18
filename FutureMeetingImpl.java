2ecca645ee0795325ae49eccf93d02fef8c56a6d
Hijiri Home <hijiribarrett@gmail.com>
2015-12-18 10:50:43 +0000

    Removed all overriding methods because it was all redundant... oops

--- FutureMeetingImpl.java
+++ FutureMeetingImpl.java
@@ -5,20 +5,5 @@ public class FutureMeetingImpl extends MeetingImpl implements FutureMeeting {
 	
 	public FutureMeetingImpl(int id, Calendar date, Set<Contact> contacts) {
 		super(id, date, contacts);
-	}	
-	
-	@Override
-	public int getId() {
-		return super.getId();
-	}
-	
-	@Override
-	public Calendar getDate() {
-		return super.getDate();
-	}
-	
-	@Override
-	public Set<Contact> getContacts() {
-		return super.getContacts();
 	}
 }
