05754cb55a9208ea1176605f93306a2f9e48bd7f
Hijiri Home <hijiribarrett@gmail.com>
2015-12-22 16:50:04 +0000

    added returnGreaterThanZeroWhenAddFutureMeeting() to test that a positive ID is always returned

--- ContactManagerShould.java
+++ ContactManagerShould.java
@@ -1,5 +1,8 @@
-import static org.junit.Assert.*;
+import java.util.Calendar;
+import java.util.Set;
+import java.util.HashSet;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Before;
 
@@ -18,4 +21,13 @@ public class ContactManagerShould {
 		assertNotNull(contactManager);
 	}
 	
+	@Test
+	public void returnGreaterThanZeroWhenAddFutureMeeting() {
+		Set<Contact> conts = new HashSet<>(); 
+		conts.add(new ContactImpl(1, "Sam", "Not nice"));
+		Calendar futureDate = Calendar.getInstance();
+		futureDate.set(3000, 12, 10);
+		
+		assertTrue(contactManager.addFutureMeeting(conts, futureDate) > 0);
+	}
 }
